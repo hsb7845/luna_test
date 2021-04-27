0838a761256569f2a4742a77affc4a61b0db915e
jooan <jooan@DESKTOP-KG1E5MB>
2021-04-27 17:25:28 +0900

    오타 수정

--- luna_test/src/main/java/com/luna/board/service/IRBoardService.java
+++ luna_test/src/main/java/com/luna/board/service/IRBoardService.java
@@ -7,7 +7,7 @@ import com.luna.board.dtos.RBoardDTO;
 public interface IRBoardService {
 	boolean insertBoard(RBoardDTO dto);
 	List<RBoardDTO> getAllList();
-	RBoardDTO getBoard(int pseq);
+	RBoardDTO getBoard(int rseq);
 	boolean deleteBoard(int rseq);
 	boolean updateBoard(RBoardDTO dto);
 	boolean mulDel(String[] chk);

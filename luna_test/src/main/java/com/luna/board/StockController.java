151c7676795c4e96dca0fdb74e3417919f55d182
HKEDU <HKEDU@DESKTOP-E8LSTNN>
2021-04-27 15:34:17 +0900

    재고현황 커밋_최서윤

--- luna_test/src/main/java/com/luna/board/StockController.java
+++ luna_test/src/main/java/com/luna/board/StockController.java
@@ -1,5 +1,80 @@
 package com.luna.board;
 
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.luna.board.dtos.StockDTO;
+import com.luna.board.service.IStockService;
+
 public class StockController {
+	
+	@Autowired
+	private  IStockService StockService;
+
+	@RequestMapping(value = "/", method = {RequestMethod.GET,RequestMethod.POST})
+	public String index(Locale locale, Model model) {
+
+		return "index";
+	}
+	
+	@RequestMapping(value = "/stock.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String rboard(Locale locale, Model model) {
+		List<StockDTO> list = StockService.getAllList();
+		model.addAttribute("list",list);
+		
+		return "stockList";
+	}
+	
+	@RequestMapping(value = "/insertStockForm.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String insertStock(Locale locale, Model model) {
+		return "insertStockForm";
+	}
+	
+	@RequestMapping(value = "/insert.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String insert(Locale locale, Model model, StockDTO dto) {
+		boolean isS = StockService.insertStock(dto);
+		if(isS) {
+			return "redirect:stock.do";
+		} else {
+			model.addAttribute("msg", "상품재고 추가를 실패하였습니다.");
+			return "error";
+		}
+	}	
+	
+	@RequestMapping(value = "/updateStockForm.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String updateStock(Locale locale, Model model,int pnum) {
+		StockDTO dto = StockService.getStock(pnum);
+		System.out.println("pnum"+dto.getPnum());
+		model.addAttribute("dto", dto);
+		return "stockUpdateForm";
+	}
+	
+	@RequestMapping(value = "/updateStock.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String update(Locale locale, Model model, StockDTO dto) {
+		boolean isS = StockService.updateStock(dto);
+		if(isS) {
+			return "redirect:stock.do";
+		}else {
+			model.addAttribute("msg","글수정실패");
+			return "error";
+		}
+	}
+	
+	@RequestMapping(value = "/muldel.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String muldel(Locale locale, Model model, String[] chk) {
+		boolean isS = StockService.mulDel(chk);
+		if(isS) {
+			return "redirect:stock.do";
+		}else {
+			model.addAttribute("msg","글수정실패");
+			return "error";
+		}
+
+	}
 
 }
--- luna_test/src/main/java/com/luna/board/service/IStockService.java
+++ luna_test/src/main/java/com/luna/board/service/IStockService.java
@@ -1,5 +1,17 @@
 package com.luna.board.service;
 
+import java.util.List;
+
+import com.luna.board.dtos.StockDTO;
+
 public interface IStockService {
+
+	List<StockDTO> getAllList();
+	boolean insertStock(StockDTO dto);
+	StockDTO getStock(int pnum);
+	boolean deleteStock(int pnum);
+	boolean updateStock(StockDTO dto);
+	boolean mulDel(String[] chk);
+
 	
 }

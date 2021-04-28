5c3a0ec7b83f2602540c3ffad367aa2f87008296
user <user@LAPTOP-MQE17IO8>
2021-04-28 15:48:55 +0900

    이벤트 게시판 내용 추가

--- luna_test/src/main/java/com/luna/board/daos/IEBoardDAO.java
+++ luna_test/src/main/java/com/luna/board/daos/IEBoardDAO.java
@@ -1,5 +1,21 @@
 package com.luna.board.daos;
 
+import java.util.List;
+
+import com.luna.board.dtos.EBoardDTO;
+
 public interface IEBoardDAO {
+
+	boolean insertBoard(EBoardDTO dto);
+
+	List<EBoardDTO> getAllList();
+
+	EBoardDTO getBoard(int eseq);
+
+	boolean deleteBoard(int eseq);
+
+	boolean updateBoard(EBoardDTO dto);
+
+	boolean mulDel(String[] chks);
 
 }

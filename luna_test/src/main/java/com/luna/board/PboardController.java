79b272b6eae71219996451fff929ca0cedff85a4
jooan <jooan@DESKTOP-KG1E5MB>
2021-05-21 14:00:56 +0900

    페이징 수정

--- luna_test/src/main/java/com/luna/board/PboardController.java
+++ luna_test/src/main/java/com/luna/board/PboardController.java
@@ -76,12 +76,12 @@ public class PboardController {
 		int total = pBoardService.countBoard();
 		if(nowPage ==null &&cntPerPage == null&&arrayNum==null) {
 			nowPage="1";
-			cntPerPage = "9";
+			cntPerPage = "11";
 			arrayNum ="1";
 		}else if(nowPage==null) {
 			nowPage="1";
 		}else if(cntPerPage==null) {
-			cntPerPage = "9";
+			cntPerPage = "11";
 		}else if(arrayNum==null) {
 			arrayNum="1";
 		}
